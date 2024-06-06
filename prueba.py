a4d69aa1ec5039263aeea430559c235c0f2e0e4d
Fernando Urruchurtu <fernandou127@gmail.com>
2024-06-05 21:04:02 -0500

    First Commit

---
+++ prueba.py
@@ -0,0 +1,21 @@
+Edad=18
+Nombre="Angel"
+Alto=True
+Estatura=1.67
+print("Edad",Edad,"Nombre",Nombre,"Alto?",Alto,"Estatura",Estatura)
+
+Numero1=4
+Numero2=6
+Suma=Numero1+Numero2
+Resta=Numero1-Numero2
+Potencia=Numero1**Numero2
+print("Numero1",Numero1,"Numero2",Numero2,"Suma=",Suma)
+print("Numero1",Numero1,"Numero2",Numero2,"Resta=",Resta)
+print("Numero1",Numero1,"Numero2",Numero2,"Potencia=",Potencia)
+
+a = int(input("Ingresa numero 1"))
+b = int(input("Ingresa numero 2"))
+c = a + b
+d = a - b
+e = a % b
+print("suma",c,"resta",d,"modulo",e)
